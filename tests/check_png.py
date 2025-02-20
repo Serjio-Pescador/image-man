8d9bb958c7b3de305f0b49e2ca834e9bfea9e291
Serjio Pescador <serjio.pescador@gmail.com>
2025-02-20 23:58:22 +0300

    compare 2 images

--- tests/check_png.py
+++ tests/check_png.py
@@ -14,19 +14,26 @@ from io import BytesIO
 load_dotenv()
 
 host_url = os.getenv("HOST")
+static_url = os.getenv("STATIC_URL")
 
-
-def test_placeholder_preset():
+def test_placeholder_preset(image_snapshot):
     # page.set_default_timeout(120000)
     # logging.info("uuid: %s", name_uuid)
 
-    query_tail = f"presetId=3312&width=720"
+    query_tail = f"presetId=3380&width=720"
 
     image_manager_url = f"{host_url}f9985145-c6d7-4d83-a85a-7c9cda10907f?{query_tail}"
-    logging.info("static url: %s", image_manager_url)
+    # logging.info("static url: %s", image_manager_url)
 
     response = requests.get(image_manager_url)
     test_img = Image.open(BytesIO(response.content))
-    with open("response.png", "wb") as f:
+    with open("response_im.png", "wb") as f:
         f.write(response.content)
     assert response.ok
+
+    stat_img_url = f"{static_url}f9985145-c6d7-4d83-a85a-7c9cda10907f?{query_tail}"
+    stat_img = requests.get(stat_img_url).content
+    with open("response_st.png", "wb") as f:
+        f.write(stat_img)
+
+    image_snapshot(Image.open(BytesIO(stat_img)), f"response_im.png", 0.2)
