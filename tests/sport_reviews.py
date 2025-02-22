b37b114d5e068d9711de4bb13fe2952f2b60b40e
Serjio Pescador <serjio.pescador@gmail.com>
2025-02-22 14:31:06 +0300

    refactor utils, function response and all tests

--- tests/sport_reviews.py
+++ tests/sport_reviews.py
@@ -3,12 +3,9 @@ import pytest
 import os
 from dotenv import load_dotenv
 import logging
-import requests
-import PIL
-# from playwright.sync_api import Page
-from utils import make_screenshot, compare_screenshot
-from utils import maker_of_test_data, make_new_url_tail_rounded_width
-from utils import check_response
+from utils.utils import make_screenshot, compare_screenshot
+from utils.utils import maker_of_test_data, make_new_url_tail_rounded_width
+from utils.receive_ok_response import check_response
 from static.test_uuid import PresetData, WidthSportReview
 
 
@@ -28,33 +25,28 @@ class TestSportReviews:
                              data
                              )
     def test_sport_preset(self, image_snapshot, preset, review_uuid, required_width):
-        # page.set_default_timeout(120000)
         logging.info("uuid: %s", review_uuid)
 
         if required_width < 350:
             scale_factor = 1
         else:
             scale_factor = 2
+
         query_tail = f"presetId={preset}&width={required_width}&scale={scale_factor}&quality=80&mediaType=webp"
-        query_im, response_width = make_new_url_tail_rounded_width(query_tail)
+        query_alphabet = f"mediatype=webp&presetid={preset}&quality=80&scale={scale_factor}&width={required_width}"
+        query_im, response_width = make_new_url_tail_rounded_width(query_alphabet)
 
         image_manager_url = f"{host_url}{review_uuid}?{query_im}"
-        stat_img_url = f"{static_url}{review_uuid}?{query_tail}"
-
-        logging.info("static url: %s", stat_img_url)
         logging.info("IM url: %s", image_manager_url)
 
-        response = requests.get(image_manager_url)
-        while check_response(response) != 200:
-            response = requests.get(image_manager_url)
-        assert response.ok
+        stat_img_url = f"{static_url}{review_uuid}?{query_tail}"
+        logging.info("static url: %s", stat_img_url)
+
+        response_im = check_response(image_manager_url)
 
         name_file = f"uuid_{review_uuid}_{preset}_width_{required_width}"
-        make_screenshot(response, img_uuid=name_file, required_width=response_width)
+        make_screenshot(response_im, img_uuid=name_file, required_width=response_width)
 
-        response_static = requests.get(stat_img_url)
-        while check_response(response_static) != 200:
-            response_static = requests.get(stat_img_url)
-        assert response_static.ok
+        response_static = check_response(stat_img_url)
 
-        compare_screenshot(response_static.content, image_snapshot, img_uuid=name_file, required_width=response_width)
+        compare_screenshot(response_static, image_snapshot, img_uuid=name_file, required_width=response_width)
---
+++ utils/receive_ok_response.py
@@ -0,0 +1,26 @@
+import logging
+import pytest
+import time
+import requests
+
+
+def check_response(url):
+
+    response = requests.get(url)
+
+    while response.status_code != 200:
+        if response.status_code == 404:
+            logging.warning("404, Image not found.")
+            pytest.skip("Image not found")
+        elif response.status_code == 403:
+            pytest.xfail("403, Access denied")
+        elif response.status_code == 429 or response.status_code == 500:
+            time.sleep(2)
+        else:
+            return response
+        response = requests.get(url)
+    assert response.ok
+    return response
+
+
+
