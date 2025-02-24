f3617c1a72d4d10b9dd9c3f238f18ce524de5f21
Serjio Pescador <serjio.pescador@gmail.com>
2025-02-24 19:47:36 +0300

    add draft confest and logging.conf

--- conftest.py
+++ conftest.py
@@ -11,6 +11,32 @@ def temp_dir(tmpdir):
     return
 
 
+def pytest_addoption(parser):
+    parser.addoption("--slow-first",
+                     action="store_true",
+                     default=False,
+                     help="run slow tests first")
+
+    parser.addoption("--slow-last",
+                     action="store_true",
+                     default=False,
+                     help="run slow tests last")
+
+
+def pytest_collection_modifyitems(config, items):
+    if config.getoption("--slow-first"):
+        run_first = pytest.mark.order("first")
+        for item in items:
+            if item.get_closest_marker("slow"):
+                item.add_marker(run_first)
+
+    elif config.getoption("--slow-last"):
+        run_last = pytest.mark.order("last")
+        for item in items:
+            if item.get_closest_marker("slow"):
+                item.add_marker(run_last)
+
+
 # Clean up: Remove the temporary files after the tests
 # @pytest.fixture(autouse=True)
 # def cleanup_temp_files(temp_dir):
